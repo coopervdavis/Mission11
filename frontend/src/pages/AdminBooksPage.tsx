2e80c7751456358a543e3dc993889b83e4e7ef55
Cooper <coopdav4@gmail.com>
2025-04-06 23:20:49 -0600

    hopefully now

--- frontend/src/pages/AdminBooksPage.tsx
+++ frontend/src/pages/AdminBooksPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { DeleteBook, fetchBooks } from "../api/BooksApi";
 import { Book } from "../types/Book";
 import NewBookForm from "../components/NewBookForm";
